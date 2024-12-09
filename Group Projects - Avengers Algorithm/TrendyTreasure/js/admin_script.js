017f4cc0b90f9ba0d630b99bce2ca9a81bdb2175
Homayra Anjum Tamanna <anjumtamanna357@gmail.com>
2024-12-09 23:34:14 +0600

    modify9 admin_script.js

--- Group Projects - Avengers Algorithm/TrendyTreasure/js/admin_script.js
+++ Group Projects - Avengers Algorithm/TrendyTreasure/js/admin_script.js
@@ -4,19 +4,16 @@ document.querySelector('#user-btn').onclick = () =>{
    profile.classList.toggle('active')
    navbar.classList.remove('active');
 }
-
 let navbar = document.querySelector('.header .flex .navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
    profile.classList.remove('active');
 }
-
 window.onscroll = () =>{
    profile.classList.remove('active');
    navbar.classList.remove('active');
 }
-
 subImages = document.querySelectorAll('.update-product .image-container .sub-images img');
 mainImage = document.querySelector('.update-product .image-container .main-image img');
 
